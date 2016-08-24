4cfbea0d30eb82d278798c3e99c34c929ff12f7e
River <wuweihua831@163.com>
2016-08-24 19:28:58 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 /*数据库配置*/
-$host = "182.61.53.3"; //MYSQL主机
+$host = "52.41.33.64"; //MYSQL主机
 $port = 3306; //MYSQL主机
 $user = "root"; //MYSQL用户
-$pwd ="12qwaszx"; //MYSQL密码
+$pwd ="wuWEIhua831"; //MYSQL密码
 $dbname = "ov"; //数据库名
 $adminuser="admin";//管理员用户名
 $adminpass="12qwaszx";//管理员密码
