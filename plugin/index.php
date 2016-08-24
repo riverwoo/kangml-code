c32ce918094d78af66418d1f719a21623c0d7a00
River <wuweihua831@163.com>
2016-08-24 11:39:45 +0800

    Add files via upload

---
+++ plugin/index.php
@@ -0,0 +1,6 @@
+<?php
+
+if (!defined('IN_IA')) {
+    exit('Access Denied');
+}
+global $_W, $_GPC;
---
+++ plugin/phb.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!defined('IN_IA')) {
+    exit('Access Denied');
+}
+/* 积分排行 */
+global $_W, $_GPC;
+
+
+$limitsum = 10; //显示多少个排行
+
+$sql = "SELECT * FROM " . tablename('ewei_shop_member')." WHERE uniacid = :uniacid ORDER BY credit1 DESC LIMIT {$limitsum}";
+
+//$sql = "SELECT a.credit1 as jf,b.* FROM " . tablename('mc_members')." a INNER JOIN " . tablename('ewei_shop_member')." b ON a.uid = b.uid WHERE a.uniacid = :uniacid ORDER BY a.credit1 DESC LIMIT {$limitsum}";
+$params = array(':uniacid' => $_W['uniacid']);
+$list = pdo_fetchall($sql, $params);
+
+//print_r('<pre>');print_r($list);exit;
+
+include $this->template('member/phb');
