c3e31ad3be4131732aadb36d05758112d946377e
River <wuweihua831@163.com>
2016-08-24 13:03:14 +0800

    Add files via upload

---
+++ user/jq/reg.php
@@ -0,0 +1,77 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <title>H+ 后台主题UI框架 - 注册</title>
+    <meta name="keywords" content="H+后台主题,后台bootstrap框架,会员中心主题,后台HTML,响应式后台">
+    <meta name="description" content="H+是一个完全响应式，基于Bootstrap3最新版本开发的扁平化主题，她采用了主流的左右两栏式布局，使用了Html5+CSS3等现代技术">
+
+    <link rel="shortcut icon" href="favicon.ico"> <link href="css/bootstrap.min.css?v=3.3.6" rel="stylesheet">
+    <link href="./css/font-awesome.min.css?v=4.4.0" rel="stylesheet">
+    <link href="./css/plugins/iCheck/custom.css" rel="stylesheet">
+    <link href="./css/animate.min.css" rel="stylesheet">
+    <link href="./css/style.min.css?v=4.1.0" rel="stylesheet">
+    <script>if(window.top !== window.self){ window.top.location = window.location;}</script>
+
+</head>
+
+<body>
+
+    <div class="middle-box text-center loginscreen   animated fadeInDown">
+        <div>
+            <div>
+
+<img src="http://q1.qlogo.cn/g?b=qq&nk=1319188427&s=100&t=" class="img-circle" alt="Cinque Terre">
+
+            </div>
+            <h3>欢迎注册 免流云</h3>
+            <p>创建一个免流云账户</p>
+            <form class="m-t" role="form" action="login.html">
+                <div class="form-group">
+                    <input type="text" class="form-control" name="user" placeholder="请输入用户名" required="">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" name="pd" placeholder="请输入密码" required="">
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="ll" placeholder="赠送流量" value="0.1" readonly>
+                </div>
+<div class="form-group">
+	<select class="form-control" name="i">
+		<option value="3">创建此账号</option>
+		 </select>
+</div>
+
+                
+                <button type="submit" class="btn btn-primary block full-width m-b">赶 快 注 册</button>
+
+                <p class="text-muted text-center"><small>已经有账户了？</small><a href="index.php">点此查询</a>
+                </p>
+
+            </form>
+        </div>
+    </div>
+    	<hr width="50%" />
+	<p class="text-center">Copyright ©2013-2016  <a class="" href="http://www.sulekeji.top">sulekeji.top</a> All Rights Reserved. 免流云  版权所有</p>
+	
+    <script src="./js/jquery.min.js?v=2.1.4"></script>
+    <script src="./js/bootstrap.min.js?v=3.3.6"></script>
+    <script src="http://www.zi-han.net/theme/hplus/js/plugins/iCheck/icheck.min.js"></script>
+    <script>
+        $(document).ready(function(){$(".i-checks").iCheck({checkboxClass:"icheckbox_square-green",radioClass:"iradio_square-green",})});
+    </script>
+    <script type="text/javascript" src="http://tajs.qq.com/stats?sId=9051096" charset="UTF-8"></script>
+    
+    
+    
+    
+    
+</body>
+
+</html>
