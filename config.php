dcbd92fd74152705f5b272ca3a74813966c78c80
River <wuweihua831@163.com>
2016-08-26 11:46:10 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /*数据库配置*/
-$host = "ec2-54-163-238-73.compute-1.amazonaws.com"; //MYSQL主机
-$port = 5432; //MYSQL主机
-$user = "zpcgxhlcawycnh"; //MYSQL用户
-$pwd ="wuWEIhua831"; //MYSQL密码
-$dbname = "d5lg99qtq10nid"; //数据库名
+$host = "db4free.net"; //MYSQL主机
+$port = 3306; //MYSQL主机
+$user = "kangml"; //MYSQL用户
+$pwd ="kangml"; //MYSQL密码
+$dbname = "kangml"; //数据库名
 $adminuser="admin";//管理员用户名
-$adminpass="12qwaszx";//管理员密码
+$adminpass="qwerty";//管理员密码
 ?>
